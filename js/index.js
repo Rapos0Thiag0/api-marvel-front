f90df6a2bf346ccd3d1a06bbdab30baa4e9eebee
ThiagoRaposo <thiago@ciclix.com>
2021-11-21 15:21:43 -0300

    inserção das imagens no homePage
    Co-authored-by: lmortola@outlook.com <lmortola@outlook.com>

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const url = "https://growdev-first-app.herokuapp.com/personagem";
+const url = "https://marvelapr.herokuapp.com/";
 let currentPage = 1;
 let lastPage;
 
@@ -14,7 +14,7 @@ function listaPersonagem(pagina) {
   document.querySelector("#seguinte").textContent = currentPage + 1;
 
   axios
-    .get(url, {
+    .get(`${url}personagens`, {
       params: {
         page: currentPage,
         limit: 15,
@@ -24,14 +24,42 @@ function listaPersonagem(pagina) {
       return response.data;
     })
     .then((data) => {
-      document.querySelector("tbody").innerHTML = "";
+      document.querySelector("#col-card").innerHTML = "";
 
       lastPage = data.totalPages;
 
       data.personagens.forEach((personagem) => {
-        document.querySelector("tbody").innerHTML += `
-                
-                `;
+        document.querySelector("#col-card").innerHTML += `
+        <div id="char-card" class="p-1 mb-2 rounded-2">
+        <a
+        href="../html/descricao.html"
+        data-bs-toggle="tooltip"
+        data-bs-placement="top"
+        title=${personagem.nome}
+        target="_black"
+      >
+        <div class="card bg-light p-1 text-white" style="diplay: inline-block;">
+          <img
+            id="img-card"
+            src=${personagem.image.path}.${personagem.image.extension}
+            class="card-img"
+            alt="personagem #"
+          />
+          <div
+            class="
+              d-flex
+              justify-content-center
+              align-items-end
+              card-img-overlay
+              p-1
+            "
+          >
+            <h5 class="card-text">${personagem.nome} </h5>
+          </div>
+        </div>
+      </a>
+      </div>
+        `;
       });
     });
 }
